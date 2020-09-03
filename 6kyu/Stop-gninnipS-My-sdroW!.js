31393402236405cccc4645d57a2949cb4c17c36d
Roy Gutierrez <roymg29@gmail.com>
2020-09-03 10:16:04 -0400

    modified stop-spinning-words

--- 6kyu/Stop-gninnipS-My-sdroW!.js
+++ 6kyu/Stop-gninnipS-My-sdroW!.js
@@ -2,13 +2,14 @@
 
 // Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
 
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+//https://www.codewars.com/kata/5264d2b162488dc400000001/train/javascript
+
 function spinWords(words) {
   let str = words.split(" ");
-  let letters = words.split("");
-  let lettersCount = 0;
   let sentence = "";
-  let bigWords = "";
-  let resultStr = "";
 
   for (let i = 0; i < str.length; i++) {
     if (str[i].length < 5) {
