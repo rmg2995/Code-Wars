4ff2e0c2bc04e1c1a741b11b1f320af40bbede15
Roy Gutierrez <roymg29@gmail.com>
2020-09-06 21:16:28 -0400

    test.py done

---
+++ 8kyu/test.py
@@ -0,0 +1,7 @@
+# Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+# https://www.codewars.com/kata/55a5bfaa756cfede78000026/train/python
+
+def problem(a):
+    #Easy Points ^_^
+    return "Error" if a == str(a) else a*50+6
