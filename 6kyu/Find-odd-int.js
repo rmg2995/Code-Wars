6e11fced69e596b517cd1ae4900362ac9eb2ce84
Roy Gutierrez <roymg29@gmail.com>
2020-09-16 19:28:25 -0400

    another solution for find odd int

--- 6kyu/Find-odd-int.js
+++ 6kyu/Find-odd-int.js
@@ -16,3 +16,21 @@ function findOdd(A, n) {
     }
   }
 }
+
+// function findOdd(arr) {
+//   var result, num = 0;
+
+//   arr = arr.sort();
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === arr[i+1]) {
+//       num++;
+//     } else {
+//       num++;
+//       if (num % 2 != 0) {
+//         result = arr[i];
+//         break;
+//       }
+//     }
+//   }
+//   return result;
+// }
