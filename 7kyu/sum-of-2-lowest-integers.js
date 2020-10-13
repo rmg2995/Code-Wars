d90842ec163fc50391a283636ddc60a76f8c4d14
Roy Gutierrez <roymg29@gmail.com>
2020-10-13 09:36:55 -0400

    sum of two smallest integers

---
+++ 7kyu/sum-of-2-lowest-integers.js
@@ -0,0 +1,25 @@
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+// https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/javascript
+function sumTwoSmallestNumbers(numbers) {
+  numbers = numbers.sort(function (a, b) {
+    return a - b;
+  });
+  return numbers[0] + numbers[1];
+}
+
+//   function sumTwoSmallestNumbers(numbers) {
+//     let lowestNumber = Math.min.apply(null, numbers.filter(Boolean));
+//     let secondNumber =  Math.min.apply(null, numbers.filter(n => n != lowestNumber))
+//   //   for (let i = 0; i < numbers.length; i++){
+//   //     console.log(Math.min(numbers[i]))
+//   //   }
+//     console.log(lowestNumber + secondNumber)
+//     return lowestNumber + secondNumber
+
+//     //Code here
+//   }
