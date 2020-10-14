e9658c3746bfe663b2cde1c6b165dfe139aa3d23
Roy Gutierrez <roymg29@gmail.com>
2020-10-14 13:56:55 -0400

    merge arrays done

---
+++ 7kyu/merge-array.js
@@ -0,0 +1,27 @@
+// Write a function that combines two arrays by alternatingly taking elements from each array in turn.
+
+// Examples:
+
+// [a, b, c, d, e], [1, 2, 3, 4, 5] becomes  [a, 1, b, 2, c, 3, d, 4, e, 5]
+
+// [1, 2, 3], [a, b, c, d, e, f] becomes [1, a, 2, b, 3, c, d, e, f]
+// Points:
+
+// The arrays may be of different lengths, with at least one character/digit.
+// One array will be of string characters (in lower case, a-z), a second of integers (all positive starting at 1).
+
+// https://www.codewars.com/kata/583af10620dda4da270000c5/train/javascript
+
+function mergeArrays(a, b) {
+  let result = [];
+
+  let i,
+    l = Math.min(a.length, b.length);
+
+  for (i = 0; i < l; i++) {
+    result.push(a[i], b[i]);
+  }
+
+  result.push(...a.slice(l), ...b.slice(l));
+  return result;
+}
